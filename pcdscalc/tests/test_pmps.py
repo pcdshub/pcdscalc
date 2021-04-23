ada3e3d51dcfe91db101bcdd97c836a74edb214f
Zachary Lentz <zlentz@slac.stanford.edu>
2021-04-23 15:33:30 -0700

    STY: placate flake8 bracket police

--- pcdscalc/tests/test_pmps.py
+++ pcdscalc/tests/test_pmps.py
@@ -9,7 +9,7 @@ from pcdscalc.pmps import (LFE, KFE, select_bitmask_boundaries,
 logger = logging.getLogger(__name__)
 
 # 32 bits, using numbers from 1 to 32
-test_boundaries = list(range(1,33))
+test_boundaries = list(range(1, 33))
 
 # Define some utility bitmasks
 allow_none = 0
@@ -38,8 +38,7 @@ def test_select_bounds(test_input, expected):
      (15.5, 100, True,   0b1111_1111_1111_1111_0000_0000_0000_0000),
      (14.5, 21.5, True,  0b0000_0000_0001_1111_1000_0000_0000_0000),
      (14.5, 21.5, False, 0b1111_1111_1100_0000_0011_1111_1111_1111),
-     (15, 20, True,      0b0000_0000_0000_1111_1000_0000_0000_0000),
-    ])
+     (15, 20, True,      0b0000_0000_0000_1111_1000_0000_0000_0000)])
 def test_get_bitmask(lower, upper, allow, expected):
     logger.debug(f'test_get_bitmask({lower}, {upper}, {allow}, {expected})')
     bitmask = get_bitmask(lower, upper, allow, 'tst', bounds=test_boundaries)
@@ -53,8 +52,7 @@ def test_get_bitmask(lower, upper, allow, expected):
      (0, bm1, True), (16, bm1, False), (30, bm1, True), (40, bm1, False),
      (0, bm2, False), (16, bm2, True), (30, bm2, False), (-1, bm2, False),
      (7, bm3, False), (16, bm3, False), (16.5, bm3, True), (17, bm3, False),
-     (15.5, bm4, True), (16, bm4, False), (16.5, bm4, False), (17, bm4, False),
-    ])
+     (15, bm4, True), (16, bm4, False), (16.5, bm4, False), (17, bm4, False)])
 def test_check_bitmask(energy, bitmask, expected):
     logger.debug(f'test_check_bitmask({energy}, {bitmask}, {expected}')
     ok = check_bitmask(energy, bitmask, 'tst', bounds=test_boundaries)
@@ -68,8 +66,7 @@ def test_check_bitmask(energy, bitmask, expected):
      (10.5, 20.5, True, (11, 20)),
      (11, 21, False, (11, 21)),
      (9.5, 15.5, False, (9, 16)),
-     (10.4, 10.6, True, (10.4, 10.4)),
-    ])
+     (10.4, 10.6, True, (10.4, 10.4))])
 def test_actual_range(lower, upper, allow, expected):
     span = check_actual_range(lower, upper, allow, 'tst',
                               bounds=test_boundaries)
